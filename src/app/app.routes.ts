c3fbc29c17f777e5981eff99237367613eedb27a
giorgizankaidze <giorgi.zankaidze@optio.ai>
2024-05-17 12:10:57 +0400

    feat: TA

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ValueAccessorComponent } from './value-accessor/value-accessor.component';
+import { ValueAccessorModule } from './value-accessor/value-accessor.module';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+];
